7b70f921dfddacbe3b90c475a85b9575b9208e66
Nik79 <niknikcor@gmail.com>
2018-03-01 21:02:21 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -7,76 +7,27 @@ module.exports = function makeExchange(currency) {
 	if (currency > 10000) {
 		return { error: "You are rich, my friend! We don't have so much coins for exchange" };
 	}
-	var hResult;
-	var qResult;
-	var dResult;
-	var nResult;
-	var pResult;
+	
+	var currencies = {
+		"H":50,
+		"Q":25,
+		"D":10,
+		"N":5,
+		"P":1
+	};
 
+	var current = currency;
 	var result = {};
 
-
-	hResult = CalculateCoins(currency);
-
-	if (hResult != undefined) {
-		result.H = hResult[1];
-		qResult = CalculateCoins(hResult[2]);
+	for(var key in currencies)
+	{
+		if(current >= currencies[key])
+		{
+			var value = Math.floor(current/currencies[key]);
+			result[key] = value;
+			current = current - value*current[key];
+		}
 	}
-
-	if (qResult != undefined) {
-		result.Q = qResult[1];
-		dResult = CalculateCoins(qResult[2]);
-	}
-
-	if (dResult != undefined) {
-		result.D = dResult[1];
-		nResult = CalculateCoins(dResult[2]);
-	}
-
-	if (nResult != undefined) {
-		result.N = nResult[1];
-		pResult = CalculateCoins(nResult[2]);
-	}
-
-	if (pResult != undefined) {
-		result.P = pResult[1];
-	}
-
+	
 	return result;
-}
-
-function CalculationHelper(amount, dividor, dividorName) {
-	if (amount >= dividor) {
-		var rest = amount % dividor;
-		var coins = (amount - rest) / dividor;
-
-		return [dividorName, coins, rest];
-	} else {
-		return undefined;
-	}
-}
-
-function CalculateCoins(amount) {
-
-	if (amount >= 50) {
-		return CalculationHelper(amount, 50, "H");
-	}
-
-	if (amount >= 25) {
-		return CalculationHelper(amount, 25, "Q");
-	}
-
-	if (amount >= 10) {
-		return CalculationHelper(amount, 10, "D");
-	}
-
-	if (amount >= 5) {
-		return CalculationHelper(amount, 5, "N");
-	}
-
-	if (amount >= 1) {
-		return CalculationHelper(amount, 1, "P");
-	}
-
-	return undefined;
 }
